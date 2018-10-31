36a0aa0e31b7cd87bfd91ec6ea746e34ccb00607
Gabriel Voicu <voicu_gabriel@ymail.com>
2018-10-31 17:56:34 +0200

    Fixed coding style based on rubocop

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,10 +1,9 @@
 class Users::ConfirmationsController < Devise::ConfirmationsController
 
-  private
+private
 
   def after_confirmation_path_for(resource_name, resource)
     sign_in(resource)
     root_path
   end
-
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,14 +1,12 @@
 class Users::SessionsController < Devise::SessionsController
-
   def new
     super
   end
 
-  protected
+protected
 
   def after_sign_in_path_for(resource)
     flash[:success] = "Welcome back #{resource.first_name}!"
     root_path
   end
-
 end
